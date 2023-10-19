4fbad77db66d97ccc34c13e180fc5dc58dfee964
joe Defendre <jchacker5@gmail.com>
2023-10-18 20:19:47 -0400

    [] bug fixes

--- CPU.java
+++ CPU.java
@@ -16,4 +16,19 @@ public class CPU {
     }
 
     public void setRunningJob(Job job) {
-   
+        this.job = job;
+    }
+
+    public boolean isEmpty() {
+        return job == null;
+    }
+
+    public void removeJob() {
+        this.job = null;
+    }
+    
+    public void execute() {
+        if(job!=null)
+            job.execute();
+    }
+}
